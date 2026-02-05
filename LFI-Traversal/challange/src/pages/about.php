2913fccb89b5b18a6e0348ebb8e878e68f19cfd8
Kaguai10 <arifkaguai10@gmail.com>
2026-02-05 11:22:50 +0700

    Create about.php

---
+++ LFI-Traversal/challange/src/pages/about.php
@@ -0,0 +1,1 @@
+<h2>About</h2><p>mail:kaguai10@mail.kaguai.net</p>                                                                                                                                                    
